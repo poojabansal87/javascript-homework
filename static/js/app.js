a85e299a776d40bdb01e4a4c79c0684b23429acd
Pooja <poojabansal87@gmail.com>
2018-11-01 23:44:25 -0700

    Added code to render table reading JS data and filter on date

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,41 @@
 // from data.js
-var tableData = data;
+var tableData = [];
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// intial load
+tableData = data;
+populateTable(tableData);
+
+function populateTable(tableDataArr) {
+
+    tbody.html("");
+
+    tableDataArr.forEach((UFOReport) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+}
+
+function filterData() {
+    var datetimeInput = document.getElementById('datetime').value;
+
+    // we will manipulate table data
+    tempData = data;
+
+    if(datetimeInput === '') {
+        filteredData = tempData;
+    } else {
+        var filteredData = tempData.filter(function(item) {
+            return datetimeInput == item.datetime;
+        });
+    }
+    
+    // repopulate table with new Data
+    populateTable(filteredData);
+}
+
